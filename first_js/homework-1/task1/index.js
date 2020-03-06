14009d5110916451aeabed7704a4193992dad03c
Danilpaschenko <Danilpaschenko802@gmail.com>
2020-03-06 20:46:51 +0200

    fix folders mistakes

--- first_js/homework-1/task1/index.js
+++ first_js/homework-1/task1/index.js
@@ -1,11 +1,10 @@
 console.log('tusk 1');
 let name = 'Генератор защитного поля';
 let cost = '1000';
-// console.log('Выбран', '«', name, '»',);
 console.log('Выбран', '«', name,',', '»','цена за штуку',  cost, 'кредитов') ;
 cost = 2000;
 console.log('Выбран', '«', name,',', '»','цена за штуку',  cost, 'кредитов') ;
-console.log('tusk 2');
-let enterName = prompt('userName');
-let greeting = prompt('greeting');
-console.log(enterName,',',greeting);
+// console.log('tusk 2');
+// let enterName = prompt('userName');
+// let greeting = prompt('greeting');
+// console.log(enterName,',',greeting);
