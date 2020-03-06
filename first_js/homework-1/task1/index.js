8ffb0a2f1829beb8e4b0b060e5d42568d5544133
Danilpaschenko <Danilpaschenko802@gmail.com>
2020-03-06 20:49:36 +0200

    fix folders

--- first_js/homework-1/task1/index.js
+++ first_js/homework-1/task1/index.js
@@ -4,7 +4,3 @@ let cost = '1000';
 console.log('Выбран', '«', name,',', '»','цена за штуку',  cost, 'кредитов') ;
 cost = 2000;
 console.log('Выбран', '«', name,',', '»','цена за штуку',  cost, 'кредитов') ;
-// console.log('tusk 2');
-// let enterName = prompt('userName');
-// let greeting = prompt('greeting');
-// console.log(enterName,',',greeting);
--- first_js/homework-1/task2/index.js
+++ first_js/homework-1/task2/index.js
@@ -1,9 +1,3 @@
-// console.log('tusk 1');
-// let name = 'Генератор защитного поля';
-// let cost = '1000';
-// console.log('Выбран', '«', name,',', '»','цена за штуку',  cost, 'кредитов') ;
-// cost = 2000;
-// console.log('Выбран', '«', name,',', '»','цена за штуку',  cost, 'кредитов') ;
 console.log('tusk 2');
 let enterName = prompt('userName');
 let greeting = prompt('greeting');
