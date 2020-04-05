e72f43f7d8585bf86ec71e5c0f510bc19d059737
Danilpaschenko <Danilpaschenko802@gmail.com>
2020-04-05 20:09:56 +0300

    commit ad tasks

--- module_2/Additional_tasks/index.js
+++ module_2/Additional_tasks/index.js
@@ -93,8 +93,8 @@ let string;
 
 
 
-console.log(formatString("Curabitur ligula sapien, tincidunt non."));
+// console.log(formatString("Curabitur ligula sapien, tincidunt non."));
 
-console.log(formatString("Vestibulum facilisis purus nec pulvinar iaculis."));
+// console.log(formatString("Vestibulum facilisis purus nec pulvinar iaculis."));
 
-console.log(formatString("Nunc sed turpis. Curabitur a felis in nunc fringilla tristique."));
+// console.log(formatString("Nunc sed turpis. Curabitur a felis in nunc fringilla tristique."));
